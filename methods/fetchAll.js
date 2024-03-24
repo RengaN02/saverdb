10433b4fc5a1d3300562c423dbc064d157fc898c
RengaN02 <60517664+RengaN02@users.noreply.github.com>
2024-03-24 15:04:54 +0300

    Update fetchAll.js

--- methods/fetchAll.js
+++ methods/fetchAll.js
@@ -1,15 +1,13 @@
 const fs = require('fs')
 const error = require('./Errors.js')
-var oku = (files) => JSON.parse(fs.readFileSync(files,'utf8'))
-var yazdir = (files,data) => fs.writeFileSync(files,JSON.stringify(data,null,4))
+var read = (files) => JSON.parse(fs.readFileSync(files,'utf8'))
+var write = (files,data) => fs.writeFileSync(files,JSON.stringify(data,null,4))
 if(!fs.existsSync('./database.json')) {
-  yazdir('./database.json', {})
+  write('./database.json', {})
   } 
 
-
 function fetchAll() {
-    const allData = oku('./database.json')
-    return allData
+    return allData = read('./database.json')
 }
 
 module.exports = fetchAll
