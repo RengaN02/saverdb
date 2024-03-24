43e417b5911b10968043f40031c97d468755e071
RengaN02 <60517664+RengaN02@users.noreply.github.com>
2024-03-24 14:58:51 +0300

    Update clear.js

--- methods/clear.js
+++ methods/clear.js
@@ -3,7 +3,7 @@ const error = require('./Errors.js')
 var read = (files) => JSON.parse(fs.readFileSync(files,'utf8'))
 var write = (files,data) => fs.writeFileSync(files,JSON.stringify(data,null,4))
 if(!fs.existsSync('./database.json')) {
-    yazdir('./database.json', {})
+    write('./database.json', {})
     } 
 
 async function clear() {
